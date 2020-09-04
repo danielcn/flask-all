8938267e0a606bf0a4c52e6fc312d401f8ba3715
Daniel Nascimento <deo.daniel@gmail.com>
2020-09-03 22:37:32 -0300

    Adding start cache application

--- flask-cache/app.py
+++ flask-cache/app.py
@@ -32,16 +32,18 @@ config = {
     "CACHE_TYPE": "simple", # Flask-Caching related configs
     "CACHE_DEFAULT_TIMEOUT": 300
 }
+
 app = Flask(__name__)
 # tell Flask to use the above defined config
 app.config.from_mapping(config)
 cache = Cache(app)
-
 
 @app.route('/')
 @cache.cached(timeout=50)
 def index():
     return 'Cached for 50s'
 
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
 
-https://www.youtube.com/watch?v=iO0sL6Vyfps&feature=em-uploademail
+# https://www.youtube.com/watch?v=iO0sL6Vyfps&feature=em-uploademail
