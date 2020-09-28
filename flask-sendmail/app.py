9edddbba929953495b4bba944e198d67dded06c2
Daniel Nascimento <deo.daniel@gmail.com>
2020-09-27 22:38:36 -0300

    Add token confirmation

--- flask-sendmail/app.py
+++ flask-sendmail/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from send_mail import send_mail
+from generate_token import generate_unique_link
+
 
 app = Flask(__name__)
 
@@ -15,8 +17,8 @@ def submit():
 
 @app.route('/confirm', methods=['POST'])
 def confirm():
+    generate_unique_link()
     return 'token validation'
-
 
 if __name__ == '__main__':
     app.run()
