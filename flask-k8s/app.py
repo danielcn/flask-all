a9aae2ae09ad2fc61b30c6719d2b5b834ac78957
Daniel Nascimento <deo.daniel@gmail.com>
2020-11-29 23:00:56 -0300

    Recreating the structure of flask project with k8s implemented
     - Endpoints refactoring
     - Create k8s deployment definition
     - Adds unit test

---
+++ flask-k8s/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def index():
+	return 'status 200 on index', 200
+
+@app.route('/error')
+def error():
+	return 'status 500 error', 500
+
+@app.route('/success', methods=['POST'])
+def success():
+	return 'status 200 on index', 200
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
