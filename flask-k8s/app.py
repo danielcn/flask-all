11cdec3d31bb12f31ebc8026f52263f9363d0a56
Daniel Nascimento <deo.daniel@gmail.com>
2020-11-30 23:57:15 -0300

    Fixing tests

--- flask-k8s/app.py
+++ flask-k8s/app.py
@@ -2,17 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def index():
-	return 'status 200 on index', 200
+	return 'status 200 index', 200
 
 @app.route('/error')
 def error():
 	return 'status 500 error', 500
 
-@app.route('/success', methods=['POST'])
+@app.route('/success', methods=['GET'])
 def success():
-	return 'status 200 on index', 200
+	return 'status 200 sucess', 200
 
 
 if __name__ == "__main__":
